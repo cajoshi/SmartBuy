ad3749451d5a1f577941696918f5dea8a748bac3
cajoshi <40358010+cajoshi@users.noreply.github.com>
2018-10-10 15:18:34 +0530

    Update Store_data.cpp
    added functionality

--- Store_data.cpp
+++ Store_data.cpp
@@ -4,14 +4,18 @@
 #include <cstdio>
 using namespace std;
 
-
+/*
+Store class is the parent class containing number of phones
+*/
 class Store{
     int n;
 public:
     Store() : n(0){}
     void set_n(int n_){n=n_;}
 };
-
+/*
+Mobile Class inherits from Store  class and contains some of the functionality of the project
+*/
 class Mobile : public Store{
     char name[50];
     float price;
@@ -29,7 +33,9 @@ public:
 void cartAdd(int);
 void cartDisplay();
 
-
+/*
+Adds the Gives mobile to cart
+*/
 void cartAdd(int num){
     fstream file;
     file.open("Data.txt", ios::in | ios::binary);
@@ -48,7 +54,9 @@ void cartAdd(int num){
     file.write((char *)&m1, sizeof(m1));
     file.close();
 }
-
+/*
+Displays The content of the cart
+*/
 void cartDisplay(){
     Mobile m1;
     fstream file;
@@ -68,6 +76,9 @@ void cartDisplay(){
     file.close();
 }
 //------------------------------------------------
+/*
+Accept Data
+*/
 void Mobile::accept()
 {
     cout << "-------------------------" << endl;
@@ -82,6 +93,9 @@ void Mobile::accept()
 
 }
 //----------------------------------------------
+/*
+Display Data
+*/
 void Mobile::display()
 {
     cout << "-------------------------" << endl;
@@ -90,6 +104,9 @@ void Mobile::display()
     cout<<"Memory : "<<memory<<endl;
 }
 //-----------------------------------------------
+/*
+Overloaded Display data
+*/
 void Mobile::display(int cnt)
 {
     cout << "-------------------------" << endl;
@@ -98,6 +115,9 @@ void Mobile::display(int cnt)
     cout<<"  Memory : "<<memory<<endl;
 }
 //----------------------------------------------
+/*
+Creates file to store mobile database
+*/
 void createFile()
 {
     int n;
@@ -121,7 +141,9 @@ void createFile()
     fout.close();
 }
 //-----------------------------------------------
-
+/*
+Displays the mobile database stored in file
+*/
 int displayFile()
 {
     ifstream fin;
@@ -149,7 +171,40 @@ int displayFile()
     return cnt;
 }
 //----------------------------------------------------
+//----------------------------------------------------
+/*
+Modifies the Data of the Cart by searching using name, price, memory
+*/
+void modifyCart(){
+	fstream file;
+	cout << "-----------------------" << endl;
+	void Sname1();
+	void Sprice1();
+	void Smemory1();
 
+	int param;
+	cout << "-------------------------" << endl;
+	cout<<"\nSearch by - "<<endl;
+	cout<<"1. Name"<<endl;
+	cout<<"2. Price"<<endl;
+	cout<<"3. Memory"<<endl;
+	cout<<"Enter parameter by which to search : ";
+	cin>>param;
+
+	    switch(param)
+	    {
+	        case 1 :    Sname1();
+	                    break;
+	        case 2 :    Sprice1();
+	                    break;
+	        case 3 :    Smemory1();
+	                    break;
+	    }
+
+}
+/*
+Acceots the input from user to choose mobile
+*/
 void buyPhone(int cnt)
 {
     cout<<cnt << endl;
@@ -170,6 +225,9 @@ void buyPhone(int cnt)
     }
 }
 //---------------------------------------------------------------------
+/*
+Searc the Database  using name price or memory
+*/
 void search_ph()
 {
     void Sname();
@@ -197,6 +255,9 @@ void search_ph()
 
 }
 //---------------------------------------------------
+/*
+Function to search by name
+*/
 void Sname()
 {
     char name[50];
@@ -235,6 +296,9 @@ void Sname()
 
 }
 //---------------------------------------------------------
+/*
+Function to search by price
+*/
 void Sprice()
 {
     float price;
@@ -275,6 +339,9 @@ void Sprice()
 }
 //----------------------------------------------------------------------
 //---------------------------------------------------------
+/*
+Function to search by memory
+*/
 void Smemory()
 {
     int memory;
@@ -286,7 +353,7 @@ void Smemory()
     cout<<"Enter memory of phone to be searched : ";
     cin>>memory;
 
-    fin.open("Data", ios::in | ios::binary);
+    fin.open("Data.txt", ios::in | ios::binary);
 
     while(fin)
     {
@@ -314,7 +381,160 @@ void Smemory()
     }
 
 }
+//---------------------------------------------------
+/*
+Function to search cart by name
+*/
+void Sname1()
+{
+    char name[50];
+    int flag;
+    fstream fin;
+    Mobile M1;
+    flag=0;
+    cout << "-------------------------" << endl;
+    cout<<"Enter name to be searched : ";
+    cin>>name;
 
+    fin.open("Cart", ios::in | ios::out | ios::binary);
+
+    while(fin)
+    {
+        fin.read((char*) &M1, sizeof(M1));
+
+        if(fin.eof()!=0)
+            break;
+
+        if(!strcmp(M1.getName(),name))
+        {
+            cout << "-------------------------" << endl;
+            cout<<"\nPhone record found."<<endl;
+            M1.display();
+            cout << "-------------------------" << endl;
+            cout << "This data will be changed" << endl;
+            M1.accept();
+            int pos = -1 * sizeof(M1);
+            fin.seekp(pos, ios::cur);
+            fin.write((char *)&M1, sizeof(M1));
+            cout << "-------------------------" << endl;
+            cout << "Modified the data" << endl;
+            flag=1;
+            break;
+        }
+    }
+    fin.close();
+
+    if(flag==0){
+        cout << "-------------------------" << endl;
+        cout<<"Record not found."<<endl;
+    }
+
+}
+//---------------------------------------------------------
+/*
+Function to search cart by price
+*/
+void Sprice1()
+{
+    float price;
+    int flag;
+    fstream fin;
+    Mobile M1;
+    flag=0;
+    cout << "-------------------------" << endl;
+    cout<<"Enter price of phone to be searched : ";
+    cin>>price;
+
+    fin.open("Cart", ios::in | ios::binary | ios::out);
+
+    while(fin)
+    {
+        fin.read((char*) &M1, sizeof(M1));
+
+        if(fin.eof()!=0)
+            break;
+
+
+        if(M1.getPrice() == price)
+        {
+            cout << "-------------------------" << endl;
+            cout<<"\nPhone record found."<<endl;
+            M1.display();
+            cout << "-------------------------" << endl;
+            cout << "This data will be changed" << endl;
+            M1.accept();
+            int pos = -1 * sizeof(M1);
+            fin.seekp(pos, ios::cur);
+            fin.write((char *)&M1, sizeof(M1));
+            cout << "-------------------------" << endl;
+            cout << "Modified the data" << endl;
+            flag=1;
+            break;
+        }
+    }
+    fin.close();
+
+    if(flag==0){
+        cout << "-------------------------" << endl;
+        cout<<"Record not found."<<endl;
+    }
+
+}
+//----------------------------------------------------------------------
+//---------------------------------------------------------
+/*
+Function to search cart by memory
+*/
+void Smemory1()
+{
+    int memory;
+    int flag;
+    fstream fin;
+    Mobile M1;
+    flag=0;
+    cout << "-------------------------" << endl;
+    cout<<"Enter memory of phone to be searched : ";
+    cin>>memory;
+
+    fin.open("Cart", ios::in | ios::binary | ios::out);
+
+    while(fin)
+    {
+        fin.read((char*) &M1, sizeof(M1));
+
+        if(fin.eof()!=0)
+            break;
+
+
+
+        if(M1.getMemory() == memory)
+        {
+            cout << "-------------------------" << endl;
+            cout<<"\nPhone record found."<<endl;
+            M1.display();
+            cout << "-------------------------" << endl;
+            cout << "This data will be changed" << endl;
+            M1.accept();
+            int pos = -1 * sizeof(M1);
+            fin.seekp(pos, ios::cur);
+            fin.write((char *)&M1, sizeof(M1));
+            cout << "-------------------------" << endl;
+            cout << "Modified the data" << endl;
+            flag=1;
+            break;
+        }
+    }
+    fin.close();
+
+    if(flag==0){
+        cout << "-------------------------" << endl;
+        cout<<"Record not found."<<endl;
+    }
+
+}
+/*
+Delets the files created while running the program 
+*/
 void exitDeleteDatabase(){
     remove("Data.txt");
     remove("Cart");
@@ -336,7 +556,8 @@ int main()
         cout<<"3. Buy a phone."<<endl;
         cout<<"4. Search for a phone"<<endl;
         cout<<"5. View cart"<<endl;
-        cout<<"6. Exit"<<endl;
+        cout<<"6. Edit cart"<<endl;
+        cout<<"7. Exit"<<endl;
 
         cout<<"Enter your choice : ";
         cin>>ch;
@@ -357,7 +578,9 @@ int main()
                         break;
             case 5:     cartDisplay();
                         break;
-            case 6:    exitDeleteDatabase();
+            case 6:		modifyCart();
+            			break;
+            case 7:    exitDeleteDatabase();
                         break;
             default:
                         cout << "-------------------------" << endl;
@@ -365,8 +588,9 @@ int main()
 
         }
 
-    }while(ch!=6);
+    }while(ch!=7);
 
 
     return 0;
 }
+
